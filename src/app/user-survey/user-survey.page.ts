7934833f2cb6aa985761f72fa6a1deef8fb5729a
Thao Pham <thaopham@uark.edu>
2019-03-05 17:48:11 -0600

    user survey updates

--- src/app/user-survey/user-survey.page.ts
+++ src/app/user-survey/user-survey.page.ts
@@ -6,6 +6,7 @@ import { AlertController } from '@ionic/angular';
   templateUrl: './user-survey.page.html',
   styleUrls: ['./user-survey.page.scss'],
 })
+
 export class UserSurveyPage implements OnInit {
 
   constructor(public alertController: AlertController) {}
@@ -31,6 +32,29 @@ export class UserSurveyPage implements OnInit {
 
     await alert.present();
   }
+
+  async presentAlert3() {
+    const alert = await this.alertController.create({
+      header: 'Are there papers, books, towels, shoes, magazines, boxes, blankets, or other objects on the floor?',
+      subHeader: 'Answer: YES',
+      message: 'Pick up things that are on the floor. Always keep objects off the floor.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  async presentAlert4() {
+    const alert = await this.alertController.create({
+      header: 'Do you have to walk over or around wires or cords (like lamp, telephone, or extension cords)?',
+      subHeader: 'Answer: YES',
+      message: "Coil or tape cords and wires next to the wall so you can't trip over them. If needed, have an electrician put in another outlet.",
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
 
 
   ngOnInit() {
