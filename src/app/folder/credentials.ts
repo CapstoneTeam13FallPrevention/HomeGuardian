03e99bc00aad69a3f86b78b3b7c57fd6edb1d282
injigo <thaopham@uark.edu>
2019-03-12 22:04:19 -0500

    firebase testing for therapist

---
+++ src/app/folder/credentials.ts
@@ -0,0 +1,9 @@
+// Initialize Firebase
+export const firebaseConfig = {
+    apiKey: "AIzaSyDbYGsPCJmhSbUsYfQKqqwJfkaMpB1HPRU",
+    authDomain: "homeguardian-6687c.firebaseapp.com",
+    databaseURL: "https://homeguardian-6687c.firebaseio.com",
+    projectId: "homeguardian-6687c",
+    storageBucket: "homeguardian-6687c.appspot.com",
+    messagingSenderId: "599034502756"
+    };
